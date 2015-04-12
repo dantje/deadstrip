ea717cf743671e695fdf962e1b7d40666d89a409
Daniel van Gerpen <daniel@vangerpen.de>
2015-04-12 20:54:02 +0200

    graphviz: use different styles for text and data

--- tographviz.py
+++ tographviz.py
@@ -2,12 +2,27 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('map.xml')
 root = tree.getroot()
 
+datasym = []
+textsym = []
+
 print("digraph {")
 
 for section in root.findall('FILE/SECTION'):
   symbol = section.get('name').replace('.text$','').replace('.data$','')
   for depends in section:
-    depsym = depends.text.replace('.text$','').replace('.data$','')
+    if '.text$' in depends.text:
+      depsym = depends.text.replace('.text$','')
+      textsym.append(depsym)
+    if '.data$' in depends.text:
+      depsym = depends.text.replace('.data$','')
+      datasym.append(depsym)
+    
     print("  %s -> %s;" % (symbol,depsym))
+
+for ts in set(textsym):
+  print(' "%s" [shape=box];' % (ts))
+
+for ds in set(datasym):
+  print(' "%s" [shape=circle];' % (ds))
 
 print("}")
